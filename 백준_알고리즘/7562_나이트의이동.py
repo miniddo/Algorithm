7f499fb25b4ef94ac97adc2cac467a79a5c59e1c
miniddo <kg3718@naver.com>
2021-01-22 23:27:04 +0900

    나이트의 이동 2차 코드

--- 백준_알고리즘/7562_나이트의이동.py
+++ 백준_알고리즘/7562_나이트의이동.py
@@ -5,24 +5,25 @@ move = [(-1, -2), (-1, 2), (1, -2), (1, 2), (-2, -1), (-2, 1), (2, -1), (2, 1)]
 
 def dfs(start_x, start_y, end_x, end_y, chase):
 
-  if start_x == end_x and start_y == end_y:
-    return 0
+  queue = deque([[start_x, start_y]])
+  chase[start_y][start_x] = 1
 
-  flag = True
-  queue = deque([[start_x, start_y, 0]])
-
-  while flag:
+  while queue:
     v = queue.popleft()
+
+    if end_x == v[0] and end_y == v[1]:
+      return chase[v[0]][v[1]] - 1
+      
     for m in move:
       xx = v[0] + m[0]
       yy = v[1] + m[1]
 
-      if xx >= 0 and xx < l and yy >=0 and yy < l:
-        if xx == end_x and yy == end_y:
-          flag = False
-        queue.append([xx, yy, v[2]+1])
-
-  return queue[-1]
+      if xx < 0 or xx >= l or yy < 0 or yy >= l:
+        continue
+      else:
+        if chase[xx][yy] == 0:
+          chase[xx][yy] = chase[v[0]][v[1]] + 1
+          queue.append([xx, yy])
 
 
 T = int(input())
@@ -34,4 +35,4 @@ for _ in range(T):
   chase = [[0] * l for _ in range(l)]
   result = dfs(start_x, start_y, end_x, end_y, chase)
 
-  print(result[2])
+  print(result)
