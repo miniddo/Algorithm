ae89ecc5a0b663847fa12be0e524e12966e71301
miniddo <kg3718@naver.com>
2021-01-25 23:52:30 +0900

    백트랙킹 DFS 방식

--- 백준_알고리즘/백트래킹/15650_N과M(2).py
+++ 백준_알고리즘/백트래킹/15650_N과M(2).py
@@ -1,11 +1,39 @@
-from itertools import combinations
-import sys
+# from itertools import combinations
+# import sys
 
+# input = sys.stdin.readline
+# N, M = map(int, input().split())
+
+# comb = list(combinations(range(1, N+1), M))
+# comb.sort(key=lambda x:x)
+
+# for c in comb:
+#     print(' '.join(map(str, c)))
+
+import sys
 input = sys.stdin.readline
+
 N, M = map(int, input().split())
 
-comb = list(combinations(range(1, N+1), M))
-comb.sort(key=lambda x:x)
+numList = [i for i in range(1, N+1)]
+visited = [False] * N
+result = []
 
-for c in comb:
-    print(' '.join(map(str, c)))
+def dfs(x, index):
+    if x == M:
+        print(' '.join(map(str, result)))
+        return
+    for i in range(index, N):
+
+        if visited[i]:  
+            continue
+        
+        result.append(numList[i])
+        visited[i] = True
+        
+        dfs(x+1, i+1)
+        
+        result.pop()
+        visited[i] = False
+
+dfs(0, 0)
