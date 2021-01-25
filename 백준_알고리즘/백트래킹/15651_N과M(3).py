fa2d2779bc589cf7b9478f51e8e7b3b4f8cf9117
miniddo <kg3718@naver.com>
2021-01-25 23:23:17 +0900

    백트랙킹 DFS 방식

--- 백준_알고리즘/백트래킹/15651_N과M(3).py
+++ 백준_알고리즘/백트래킹/15651_N과M(3).py
@@ -1,15 +1,3 @@
-# from itertools import permutations
-# import sys
-
-# input = sys.stdin.readline
-# N, M = map(int, input().split())
-
-# number = [i for i in range(1, N+1)]
-# permu = list(permutations(number, M))
-
-# for p in permu:
-#     print(' '.join(map(str, p)))
-
 import sys
 input = sys.stdin.readline
 
