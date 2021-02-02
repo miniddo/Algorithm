0a6379a04d0df6bf2f4243309a504cf338d4106e
miniddo <kg3718@naver.com>
2021-02-02 22:17:42 +0900

    이분탐색 랜선자르기

---
+++ 백준_알고리즘/이진탐색/1654_랜선자르기.py
@@ -0,0 +1,27 @@
+# K개의 랜선
+# 모두 N개의 같은 길이의 랜선
+
+import sys
+input = sys.stdin.readline
+
+K, N = map(int, input().split())
+lan = [int(input()) for i in range(K)]
+lan.sort()
+
+start, end = 1, lan[-1]
+
+while start <= end:
+    mid = (start + end)//2
+    # print('*', start, end)
+    target = 0
+    for i in lan:
+        target += (i // mid)
+
+    if target < N:
+        end = mid - 1
+    else:   # 잘라낸 개수가 기준보다 많다면, 더 큰 단위로 자른다.(최댓값)
+        result = mid
+        start = mid + 1
+    # print('^', start, end)
+
+print(result)
