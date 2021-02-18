46868fa51e3cf58f71ddd971cde03c09f030acfc
miniddo <kg3718@naver.com>
2021-02-18 21:45:02 +0900

    개미전사

---
+++ 이것이_취업을_위한_코딩_테스트다/CHAPTER08_다이나믹프로그래밍/개미전사.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+K = list(map(int, input().split()))
+
+dp = [0] * 99
+
+dp[0] = 0
+dp[1] = max(K[0], K[1])
+
+for i in range(2, N):
+    dp[i] = max(dp[i-1], dp[i-2]+K[i])
+
+print(dp[N-1])
