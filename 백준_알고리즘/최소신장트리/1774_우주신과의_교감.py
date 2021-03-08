65b583931b9075c5f35e564bbc350896973c3f7f
miniddo <kg3718@naver.com>
2021-03-08 23:08:22 +0900

    우주신과의 교감 (진행중)

--- 백준_알고리즘/최소신장트리/1774_우주신과의_교감.py
+++ 백준_알고리즘/최소신장트리/1774_우주신과의_교감.py
@@ -15,28 +15,29 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-n = int(input())
-parent = [0] * (n+1)
-
-star = [[0, 0] for _ in range(n+1)]
+n, m = map(int, input().split())    # n: 우주신의 개수, m: 이미 연결된 통로 개수
+universe = [list(map(int, input().split())) for _ in range(n)]
+universe.insert(0, [0, 0])
 dists = []
+parent = [0] * (n+1)
 result = 0
 
 for i in range(1, n+1):
     parent[i] = i
 
-for i in range(1, n+1):
-    a, b = map(float, input().split())
-    star[i][0], star[i][1] = a, b
+for _ in range(m):
+    a, b = map(int, input().split())
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
 for i in range(1, n+1):
     for j in range(i+1, n+1):
-        # print(i, star[i], j, star[j])
-        cost = math.sqrt(math.pow(star[j][0] - star[i][0], 2) + math.pow(star[j][1] - star[i][1], 2))
+        cost = math.sqrt(math.pow(universe[j][0] - universe[i][0], 2) + math.pow(universe[j][1] - universe[i][1], 2))
         cost = round(cost, 2)
         dists.append((cost, i, j))
 
-# print(dists)
 dists.sort()
 
 for dist in dists:
@@ -45,6 +46,14 @@ for dist in dists:
         union_parent(parent, a, b)
         result += cost
 
-print(result)
 
-    
+print("%0.2f" % result)
+
+'''
+4 1
+1 1
+3 1
+2 3
+4 3
+1 4
+'''
