1e9418b9500bb24f26e49b9b629b8edb2f175fee
miniddo <kg3718@naver.com>
2021-03-20 22:19:28 +0900

    로봇청소기 성공

--- 백준_알고리즘/구현/14503_로봇청소기.py
+++ 백준_알고리즘/구현/14503_로봇청소기.py
@@ -5,54 +5,48 @@ n, m = map(int, input().split())
 r, c, d = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(n)]
 
-clean = 0
-flag = True
 
-while flag:
+def solution(r, c, d, clean):
+
     if matrix[r][c] == 0:
         clean += 1
         matrix[r][c] = 2
     
     if d == 0:  # 북쪽(위쪽)
         if matrix[r][c-1] != 0 and matrix[r][c+1] != 0 and matrix[r-1][c] != 0 and matrix[r+1][c] == 1:
-            flag = False
+            return clean
         if matrix[r][c-1] != 0 and matrix[r][c+1] != 0 and matrix[r-1][c] != 0 and matrix[r+1][c] != 0:
-            r, c = r+1, c 
-        if matrix[r][c-1] == 1:
-            d = 3
+            return solution(r+1, c, d, clean)
+        if matrix[r][c-1] == 1 or matrix[r][c-1] == 2:
+            return solution(r, c, 3, clean)
         if matrix[r][c-1] == 0:
-            d = 3
-            r, c = r, c-1
+            return solution(r, c-1, 3, clean)
     elif d == 1: # 동쪽(오른쪽)
         if matrix[r][c-1] == 1 and matrix[r][c+1] != 0 and matrix[r-1][c] != 0 and matrix[r+1][c] != 0:
-            flag = False
+            return clean
         if matrix[r][c-1] != 0 and matrix[r][c+1] != 0 and matrix[r-1][c] != 0 and matrix[r+1][c] != 0:
-            r, c = r, c-1 
-        if matrix[r-1][c] == 1:
-            d = 0
+            return solution(r, c-1, d, clean)
+        if matrix[r-1][c] == 1 or matrix[r-1][c] == 2:
+            return solution(r, c, 0, clean)
         if matrix[r-1][c] == 0:
-            d = 0
-            r, c = r-1, c
+            return solution(r-1, c, 0, clean)
     elif d == 2: # 남쪽(아래쪽)
         if matrix[r][c-1] != 0 and matrix[r][c+1] != 0 and matrix[r-1][c] == 1 and matrix[r+1][c] != 0:
-            flag = False
+            return clean
         if matrix[r][c-1] != 0 and matrix[r][c+1] != 0 and matrix[r-1][c] != 0 and matrix[r+1][c] != 0:
-            r, c = r-1, c 
-        if matrix[r][c+1] == 1:
-            d = 1
+            return solution(r-1, c, d, clean) 
+        if matrix[r][c+1] == 1 or matrix[r][c+1] == 2:
+            return solution(r, c, 1, clean)
         if matrix[r][c+1] == 0:
-            d = 1
-            r, c = r, c+1
+            return solution(r, c+1, 1, clean)
     elif d == 3: # 서쪽(왼쪽)
         if matrix[r][c-1] != 0 and matrix[r][c+1] == 1 and matrix[r-1][c] != 0 and matrix[r+1][c] != 0:
-            flag = False
+            return clean
         if matrix[r][c-1] != 0 and matrix[r][c+1] != 0 and matrix[r-1][c] != 0 and matrix[r+1][c] != 0:
-            r, c = r, c+1 
-        if matrix[r+1][c] == 1:
-            d = 2
+            return solution(r, c+1, d, clean) 
+        if matrix[r+1][c] == 1 or matrix[r+1][c] == 2:
+            return solution(r, c, 2, clean)
         if matrix[r+1][c] == 0:
-            d = 2
-            r, c = r+1, c
+            return solution(r+1, c, 2, clean)
 
-
-print(clean)
+print(solution(r, c, d, 0))
