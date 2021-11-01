3bd505d7b7d3bfa56890660bf229d40d31623a6b
miniddo <kg3718@naver.com>
2021-11-02 00:36:43 +0900

    20211101 [3차]파일명정렬

--- 프로그래머스/20211101_[3차]파일명정렬.py
+++ 프로그래머스/20211101_[3차]파일명정렬.py
@@ -2,34 +2,34 @@ def solution(files):
 
     newArr = []
     for file in files:
-        str, number = '', ''
-        for f in file:
-            if f not in '0123456789':
-                str += f
+        head, number = file[0], ''
+        point = ''
+        for f in range(1, len(file)):
+            pre = file[f-1] in '0123456789'
+            post = file[f] in '0123456789'
+            if pre == True and post == False:
+                point += str(f)
+                break
+            if file[f] not in '0123456789':
+                head += file[f]
             else:
                 if len(number) < 5:
-                    number += f
+                    number += file[f]
                 else:
-                    str += f
+                    point += str(f)
+                    break
 
-        start = file.index(number)
-        end = len(number)
-
-        head = file[0:start]
-        tail = file[start+end:]
-
-        print(number)
+        tail = file[int(point):] if point != '' else ''
         newArr.append([head, number, tail])
 
     newArr.sort(key=lambda x: (x[0].lower(), int(x[1])))
-
     answer = [''.join(new) for new in newArr]
     return answer
 
 
-# print(solution(["img12.png", "img10.png", "img02.png",
-#                 "img1.png", "IMG01.GIF", "img2.JPG"]))
-# print(solution(["F-5 Freedom Fighter", "B-50 Superfortress",
-#                 "A-10 Thunderbolt II", "F-14 Tomcat"]))
+print(solution(["img12.png", "img10.png", "img02.png",
+                "img1.png", "IMG01.GIF", "img2.JPG"]))
+print(solution(["F-5 Freedom Fighter", "B-50 Superfortress",
+                "A-10 Thunderbolt II", "F-14 Tomcat"]))
 print(solution(["img000012345", "img1.png", "img2", "IMG02"]))
-# ["img1.png","img2","IMG02", "img000012345"]
+print(solution(["cb123345678", "db123ds12"]))
