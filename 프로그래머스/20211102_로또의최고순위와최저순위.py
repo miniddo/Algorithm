1b8f554d1082988eb95ecac6166024d6a9858a8d
miniddo <kg3718@naver.com>
2021-11-02 22:35:31 +0900

    20211102 로또의 최고순위와 최저순위 ver1

---
+++ 프로그래머스/20211102_로또의최고순위와최저순위.py
@@ -0,0 +1,41 @@
+def matchScore(index, score, result):
+    if score == 6:
+        result[index] = 1
+    elif score == 5:
+        result[index] = 2
+    elif score == 4:
+        result[index] = 3
+    elif score == 3:
+        result[index] = 4
+    elif score == 2:
+        result[index] = 5
+    else:
+        result[index] = 6
+
+
+def solution(lottos, win_nums):
+
+    lowest = 0
+    for l in lottos:
+        if l in win_nums:
+            lowest += 1
+
+    highest = lowest + lottos.count(0)
+
+    result = [0, 0]
+    matchScore(0, highest, result)
+    matchScore(1, lowest, result)
+
+    return result
+
+
+print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))  # [3, 5]
+print(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25]))  # [1, 6]
+print(solution([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35]))  # [1, 1]
+
+# 1위 - 6
+# 2위 - 5
+# 3위 - 4
+# 4위 - 3
+# 5위 - 2
+# 6위 - 그외
